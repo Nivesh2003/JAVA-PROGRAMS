03c9d6537c6869a70c5c6898f19849c4f00e045c
Nivesh2003 <149897710+Nivesh2003@users.noreply.github.com>
2024-09-23 22:58:55 +0530

    Add files via upload
    Creating a thread by extending the Thread class.

---
+++ MyThread.java
@@ -0,0 +1,16 @@
+
+public class MyThread extends Thread{
+    @Override
+    public void run(){
+    for (int i = 0; i < 6; i++) {
+        System.out.println(Thread.currentThread());
+    }
+
+    }
+    public static void main(String[] args) {
+        MyThread thread1 = new MyThread();
+        MyThread thread2 = new MyThread();
+        thread1.start();
+        thread2.start();
+    }
+}
